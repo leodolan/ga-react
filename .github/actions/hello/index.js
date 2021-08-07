e0dcbfc0e34741238bbfe1c907003713734215f3
Leo Dolan <ldolan@dminc.om>
2021-08-06 23:34:47 -0400

    fefe

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,7 +2,14 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try{
+
+    core.debug('Debug Message');
+    core.warning('Warning message');
+    core.error('Error Message');
+
     const name = core.getInput('who-to-greet');
+    
+    core.setSecret(name);
 
     console.log(`Hello ${name}`);
     
@@ -10,7 +17,12 @@ try{
     
     core.setOutput("time", time.toTimeString());
     
+    core.startGroup('Logging github object');
     console.log(JSON.stringify(github, null, '\t'));
+    core.endGroup();
+    core.exportVariable('HELLO', 'hello');
+    
+
 } catch (err) {
     core.setFailed(err.message);
 }
