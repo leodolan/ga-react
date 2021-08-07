a7fd3c469c64d0a9ad5b6e26c9c2199437f8f6c7
Leo Dolan <ldolan@dminc.om>
2021-08-07 12:04:02 -0400

    fefe

--- .github/actions/issue/dist/index.js
+++ .github/actions/issue/dist/index.js
@@ -3716,31 +3716,26 @@ exports.getState = getState;
 const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
-async function run() {
-    try{
-        const token = core.getInput('token');
-        const title = core.getInput('title');
-        const body = core.getInput('body');
-        const assignees = core.getInput('assignees');
-    
-        const octokit = new github.getOctokit(token);
-    
-        const response = await octokit.rest.issues.create({
-          ...github.context.repo,
-          title,
-          body,
-          assignees: assignees ? assignees.split(',') : undefined
-        });
-    
-        core.setOutput('issue', JSON.stringify(response.data));
-    
-    } catch (err){
-        core.setFailed(err.message);
-    }
-};
+try{
+    const token = core.getInput('token');
+    const title = core.getInput('title');
+    const body = core.getInput('body');
+    const assignees = core.getInput('assignees');
 
-run();
+    const octokit = new github.getOctokit(token);
+    
+    const response = octokit.create.issues({
+      ...github.context.repo,
+      title,
+      body,
+      assignees: assignees ? assignees.split(',') : undefined
+    });
 
+    core.setOutput('issue', JSON.stringify(response.data));
+
+} catch (err){
+    core.setFailed(err.message);
+}
 
 /***/ }),
 
