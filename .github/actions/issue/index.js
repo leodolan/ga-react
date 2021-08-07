8a77b9422024d62e66cc8703f72038171445fc6c
Leo Dolan <ldolan@dminc.om>
2021-08-07 12:10:05 -0400

    fefe

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -1,23 +1,31 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try{
-    const token = core.getInput('token');
-    const title = core.getInput('title');
-    const body = core.getInput('body');
-    const assignees = core.getInput('assignees');
-
-    const octokit = new github.getOctokit(token);
+async function run() {
+    // This should be a token with access to your repository scoped in as a secret.
+    // The YML workflow will need to set myToken with the GitHub Secret Token
+    // myToken: ${{ secrets.GITHUB_TOKEN }}
+    // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+    try{
+        const token = core.getInput('token');
+        const title = core.getInput('title');
+        const body = core.getInput('body');
+        const assignees = core.getInput('assignees');
     
-    const response = octokit.rest.issues.create({
-      ...github.context.repo,
-      title,
-      body,
-      assignees: assignees ? assignees.split(',') : undefined
-    });
-
-    core.setOutput('issue', JSON.stringify(response.data));
-
-} catch (err){
-    core.setFailed(err.message);
+        const octokit = new github.getOctokit(token);
+        
+        const { data: response } = await octokit.rest.issues.create({
+          ...github.context.repo,
+          title,
+          body,
+          assignees: assignees ? assignees.split(',') : undefined
+        });
+    
+        core.setOutput('issue', JSON.stringify(response.data));
+    
+    } catch (err){
+        core.setFailed(err.message);
+    }
 }
+
+run();
